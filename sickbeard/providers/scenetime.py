170c0560139b4f0196fccab2eb1c6bba0457beff
Damian Ziemba <nazriel@dzfl.pl>
2018-06-13 07:54:50 +0200

    Fix search endpoint URL for scenetime provider.
    fixes 4755

--- sickbeard/providers/scenetime.py
+++ sickbeard/providers/scenetime.py
@@ -46,7 +46,7 @@ class SceneTimeProvider(TorrentProvider):  # pylint: disable=too-many-instance-a
         self.urls = {'base_url': 'https://www.scenetime.com',
                      'login': 'https://www.scenetime.com/takelogin.php',
                      'detail': 'https://www.scenetime.com/details.php?id=%s',
-                     'apisearch': 'https://www.scenetime.com/browse_API.php',
+                     'apisearch': 'https://www.scenetime.com/browse.php',
                      'download': 'https://www.scenetime.com/download.php/%s/%s'}
 
         self.url = self.urls['base_url']
